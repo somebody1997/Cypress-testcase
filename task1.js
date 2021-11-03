8c9aaa207b70bc7cd0d9fb8d3ee8d63db34acac7
somebody1997 <eyan505@gmail.com>
2021-11-03 14:22:15 +0800

    full-position

--- task1.js
+++ task1.js
@@ -1,13 +1,15 @@
- var btn = document.getElementById("btn");
+ var btn = document.getElementById("phantom");
+ var screenWidth = window.innerWidth;
+ var screenHeight = window.innerHeight;
 
  function hideLoadingDiv() {
     setTimeout(function(){
-      document.getElementById('btn').classList.add('hidden');
+      document.getElementById('phantom').classList.add('hidden');
     },800)
   }
 
- btn.style.top = Math.floor((Math.random() * 1000) + 1) + "px";
- btn.style.left = Math.floor((Math.random() * 1000) + 1) + "px";
+ btn.style.top = Math.floor((Math.random() * screenHeight) - 1) + "px";
+ btn.style.left = Math.floor((Math.random() * screenWidth) - 1) + "px";
 
  let count = 1;
  function myFunction() {
@@ -15,6 +17,6 @@
      count++;
    }
    else {
-     document.getElementById("passway").innerHTML = "passYa";
+     document.getElementById("passcode").innerHTML = "passYa";
    }
  };
